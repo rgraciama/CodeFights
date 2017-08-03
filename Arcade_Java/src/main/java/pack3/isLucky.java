be8e91c3f0f187909266c87d1ce5f2287b1560d6
rgraciama <r.gracia.maiques@gmail.com>
2017-08-03 23:47:32 +0200

    isLucky from Codefights

---
+++ Arcade_Java/src/main/java/pack3/isLucky.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pack3;
+
+/**
+ *
+ * @author Rulo
+ */
+public class isLucky {
+
+    boolean isLucky(int n) {
+        String s = Integer.toString(n);
+        int l = s.length();
+        int cL = 0, cF = 0;
+        for (int i = 0,j = l-1; i<l/2; i++, j--) {
+            cL += Integer.valueOf(s.charAt(i));
+            cF += Integer.valueOf(s.charAt(j));
+        }
+        if (cL == cF) return true;
+                else return false;
+    }
+
+}
